20e2ecfb42c7ac04748bc5391f77b9efc82923e2
MohamedFarag <71080118+mohame12@users.noreply.github.com>
2024-07-27 07:48:33 +0300

    first commit

---
+++ lib/features/presentation/home_screen/view/home_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+  static const String id='HomeView';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
