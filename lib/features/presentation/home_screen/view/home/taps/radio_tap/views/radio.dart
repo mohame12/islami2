c81ec7535246a29f3a1ba9f04d5182d95047d94b
SamaAbdelghfar99 <samaabdelghfar90@gmail.com>
2024-07-29 11:20:05 +0300

    Radio Tap

--- lib/features/presentation/home_screen/view/home/taps/radio_tap/views/radio.dart
+++ lib/features/presentation/home_screen/view/home/taps/radio_tap/views/radio.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class RadioTap extends StatelessWidget {
   const RadioTap({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Radio'));
+    return Column(
+      //crossAxisAlignment: CrossAxisAlignment.stretch,
+      //mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Spacer(),
+        Image.asset('assets/images/radio_image.png',height: 222,),
+        Spacer(),
+        Text('إذاعة القرآن الكريم',style: GoogleFonts.elMessiri(
+          fontSize: 20,
+          fontWeight: FontWeight.w600,
+        ),textDirection: TextDirection.rtl,
+          textAlign: TextAlign.center,
+        ),
+        Spacer(),
+        Row(
+          children: [
+            Spacer(),
+            IconButton(onPressed: (){}, icon: Icon(Icons.skip_next,color: Color(0xffB7935F),size: 33,)),
+            Spacer(),
+            IconButton(onPressed: (){}, icon: Icon(Icons.play_arrow,color: Color(0xffB7935F),size: 46,)),
+            Spacer(),
+            IconButton(onPressed: (){}, icon: Icon(Icons.skip_next_sharp,color: Color(0xffB7935F),size: 33,)),
+            Spacer(),
+
+          ],
+        ),
+        Spacer(),
+
+      ],
+    );
   }
 }
